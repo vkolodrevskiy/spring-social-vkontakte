e241252c67266858869ee0ee549b4f929944ba23
Ivan Sharamet <isharamet@gmail.com>
2013-01-29 09:20:10 +0300

    Added Jackson mix-in class for PostStatus

---
+++ spring-social-vkontakte/src/main/java/org/springframework/social/vkontakte/api/impl/json/PostStatusMixin.java
@@ -0,0 +1,24 @@
+package org.springframework.social.vkontakte.api.impl.json;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.Date;
+
+/**
+ * Mixin for {@link org.springframework.social.vkontakte.api.PostStatus}
+ * @author Ivan Sharamet
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PostStatusMixin {
+
+    @JsonCreator
+    PostStatusMixin(@JsonProperty("post_id") String postId, @JsonProperty("processing") boolean processing) {}
+
+    @JsonProperty("post_id")
+    String postId;
+
+    @JsonProperty("processing")
+    boolean processing;
+}
