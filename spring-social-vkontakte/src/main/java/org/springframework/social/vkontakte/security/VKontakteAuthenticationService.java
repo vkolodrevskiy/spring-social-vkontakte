813a94a0b769d7017e35e3339a7c110e6a0121f3
vkolodrevskiy <viktor.kolodrevskiy@gmail.com>
2014-01-31 16:17:40 +0200

    this is test commit to trigger build.

--- spring-social-vkontakte/src/main/java/org/springframework/social/vkontakte/security/VKontakteAuthenticationService.java
+++ spring-social-vkontakte/src/main/java/org/springframework/social/vkontakte/security/VKontakteAuthenticationService.java
@@ -19,6 +19,10 @@ import org.springframework.social.security.provider.OAuth2AuthenticationService;
 import org.springframework.social.vkontakte.api.VKontakte;
 import org.springframework.social.vkontakte.connect.VKontakteConnectionFactory;
 
+/**
+ * VKontakte Authentication Service.
+ * @author vkolodrevskiy
+ */
 public class VKontakteAuthenticationService extends OAuth2AuthenticationService<VKontakte> {
 
 	public VKontakteAuthenticationService(String apiKey, String appSecret) {
