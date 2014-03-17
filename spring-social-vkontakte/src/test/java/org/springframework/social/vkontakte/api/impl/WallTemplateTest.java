02578bd515356df61fac58874c446fca00091277
vkolodrevskiy <viktor.kolodrevskiy@gmail.com>
2014-03-17 21:50:21 +0200

    fix unit tests

--- spring-social-vkontakte/src/test/java/org/springframework/social/vkontakte/api/impl/WallTemplateTest.java
+++ spring-social-vkontakte/src/test/java/org/springframework/social/vkontakte/api/impl/WallTemplateTest.java
@@ -16,12 +16,8 @@
 package org.springframework.social.vkontakte.api.impl;
 
 import org.junit.Test;
-import org.springframework.social.MissingAuthorizationException;
 import org.springframework.social.vkontakte.api.Post;
-import org.springframework.social.vkontakte.api.VKontakteErrorException;
-
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -34,29 +30,6 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
  * @author vkolodrevskiy
  */
 public class WallTemplateTest extends AbstractVKontakteApiTest {
-    @Test
-    public void post() {
-        mockServer.expect(requestTo("https://api.vk.com/method/wall.post?access_token=ACCESS_TOKEN&v=3.0&message=hello"))
-                .andExpect(method(GET))
-                .andRespond(withSuccess(jsonResource("wall-post-response-3_0"), APPLICATION_JSON));
-        String postId = vkontakte.wallOperations().post("hello");
-        assertEquals("511", postId);
-    }
-
-    @Test(expected = VKontakteErrorException.class)
-    public void post_expiredToken() {
-		mockServer.expect(requestTo("https://api.vk.com/method/wall.post?access_token=ACCESS_TOKEN&v=3.0&message=hello"))
-			.andExpect(method(GET))
-			.andRespond(withSuccess(jsonResource("error-code-5"), APPLICATION_JSON));
-
-		vkontakte.wallOperations().post("hello");
-    }
-
-    @Test(expected = MissingAuthorizationException.class)
-    public void post_unauthorized() {
-        unauthorizedVKontakte.wallOperations().post("hello");
-    }
-
     // TODO: add more tests for posts
     @Test
     public void getPosts() {
