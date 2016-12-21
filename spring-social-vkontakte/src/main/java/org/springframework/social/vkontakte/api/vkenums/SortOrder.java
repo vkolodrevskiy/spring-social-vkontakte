2bfe982b4fa613921a524b3343e5ee0156f1d3f4
vkolodrevskiy <viktor.kolodrevskiy@gmail.com>
2016-12-21 03:49:41 +0200

    Remove old API implementation code.

--- spring-social-vkontakte/src/main/java/org/springframework/social/vkontakte/api/vkenums/SortOrder.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.social.vkontakte.api.vkenums;
-
-/**
- * Defines sort order for wall.getComments operation
- */
-public enum SortOrder {
-    asc,
-    desc
-}
---
+++ spring-social-vkontakte/src/test/java/org/springframework/social/vkontakte/connect/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * TODO: add tests.
+ */
+package org.springframework.social.vkontakte.connect;
